a6ca29f9e966e33f9e207024746a241c93766626
Jon Ander <jonander2233@gmail.com>
2024-12-28 18:08:04 +0100

    wip

--- Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/MainActivity.java
+++ Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/MainActivity.java
@@ -1,24 +1,83 @@
 package com.jonander2233.test01examen01;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.jonander2233.test01examen01.ui.fragments.AviableCardsFragment;
+import com.jonander2233.test01examen01.ui.fragments.ClasificationFragment;
+import com.jonander2233.test01examen01.utils.models.Competition;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private Competition competition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Configurar Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Configurar Navigation Drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Manejar navegación inicial
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AviableCardsFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_aviable_cards);
+        }
+
+        // Manejar el botón "Atrás" con OnBackPressedDispatcher
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    // Permitir el comportamiento predeterminado si no se intercepta
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
         });
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.nav_aviable_cards)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AviableCardsFragment()).commit();
+        else if (item.getItemId() == R.id.nav_clasification)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ClasificationFragment()).commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START); // Cerrar el Navigation Drawer al seleccionar un ítem
+        return true;
     }
 }
--- Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/utils/adapters/CardAdapter.java
+++ Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/utils/adapters/CardAdapter.java
@@ -7,10 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardHolder>{
-
     @NonNull
     @Override
     public CardHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         return null;
     }
 
