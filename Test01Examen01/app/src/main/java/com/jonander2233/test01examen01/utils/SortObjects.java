3fdd5955cf47aabb5b20a9cc418ed9534e83bd09
jon ander <jonander2233@gmail.com>
2025-01-05 13:03:33 +0100

    wip

--- Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/utils/SortObjects.java
+++ Test01Examen01/app/src/main/java/com/jonander2233/test01examen01/utils/SortObjects.java
@@ -1,7 +1,11 @@
 package com.jonander2233.test01examen01.utils;
 
 import com.jonander2233.test01examen01.utils.models.Card;
+import com.jonander2233.test01examen01.utils.models.Competition;
+import com.jonander2233.test01examen01.utils.models.Match;
+import com.jonander2233.test01examen01.utils.models.Player;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -9,7 +13,9 @@ import java.util.Map;
 
 public class SortObjects {
     private static Map<String, Integer> ordenRarezas;
+    private static Map<Player, Integer> victories;
     static {
+
         ordenRarezas = new HashMap<>();
         ordenRarezas.put("Champion", 5);
         ordenRarezas.put("Legendary", 4);
@@ -19,5 +25,12 @@ public class SortObjects {
     }
     public static void orderCards(List<Card> cardList){
         cardList.sort(Comparator.comparingInt(Card::getElixir).thenComparing(o -> ordenRarezas.get(o.getRarity())).thenComparing(Card::getName));
+    }
+    public static void orderPlayers(Competition competition){
+        List<Match> matches = competition.getMatches();
+        List<Player> players = competition.getPlayers();
+        for (int i = 0; i < players.size(); i++) {
+            Player actualP = players.get(i);
+        }
     }
 }
