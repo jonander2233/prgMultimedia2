c8eeb3e54e1ce0cf6702ba50da1c1ec4c6014d3c
jon ander <jonander2233@gmail.com>
2025-01-30 18:33:53 +0100

    wip

--- EjercicioPreferencias/app/build.gradle.kts
+++ EjercicioPreferencias/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.preference:preference:1.2.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
---
+++ EjercicioPreferencias/app/src/main/java/com/jonander2233/ejerciciopreferencias/OptionsAcrivity.java
@@ -0,0 +1,22 @@
+package com.jonander2233.ejerciciopreferencias;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OptionsAcrivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction().replace(R.id)
+    }
+}
+
+
+
+
+
+
+
+
